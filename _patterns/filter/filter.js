d0823c711b24bdb062ed83719821103eefc59b6d
arobillard <robi0653@algonquinlive.com>
2019-02-26 19:07:10 -0500

    Test jQuery is working

--- _patterns/filter/filter.js
+++ _patterns/filter/filter.js
@@ -1,21 +1,5 @@
 // Category Selector --------------------
 
-$('.category-selector').on('click', function (e) {
-  e.preventDefault();
-  var categoryName = $(this).attr('id');
-  var breadCrumbName = $(this).children('p').text();
-  $('.category-selector').removeClass('category-selected');
-  $(this).addClass('category-selected');
-  $('#designer').removeClass();
-  $('#designer').addClass(categoryName);
-  $('.bread-crumbs-category a').text(breadCrumbName);
-});
-
-$('.bread-crumbs-all-products').on('click', function (e) {
-  e.preventDefault();
-  $('.category-selector').removeClass('category-selected');
-  $('#category-all').addClass('category-selected');
-  $('#designer').removeClass();
-  $('#designer').addClass('category-all');
-  $('.bread-crumbs-category a').text('All');
+$('.category-selector').on('click', function() {
+  console.log('clicked');
 });
