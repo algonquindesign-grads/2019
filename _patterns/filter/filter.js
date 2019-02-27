1283e9aaf98418485efe271364f03a83a4a1d65c
ksheedy <shee0067@algonquinlive.com>
2019-02-26 19:38:52 -0500

    Get jquery working properly

--- _patterns/filter/filter.js
+++ _patterns/filter/filter.js
@@ -1,5 +1,12 @@
-// Category Selector --------------------
+$('.selector').on('click', function() {
+  var catSelect = $(this).attr('id');
 
-$('.category-selector').on('click', function() {
-  console.log('clicked');
+  $('#designer-list').removeClass('category-all');
+  $('#designer-list').removeClass('category-branding');
+  $('#designer-list').removeClass('category-illustration');
+  $('#designer-list').removeClass('category-uiux');
+  $('#designer-list').removeClass('category-web');
+  $('#designer-list').removeClass('category-motion');
+  $('#designer-list').removeClass('category-photography');
+  $('#designer-list').addClass(catSelect);
 });
